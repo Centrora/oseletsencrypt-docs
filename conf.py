66d57dfac775f3079019339477ae3913fd9b7183
helix <helix@centrora.com>
2018-09-30 22:42:32 +1000

    Update logo to white logo

--- conf.py
+++ conf.py
@@ -144,7 +144,7 @@ html_theme_path = ["themes"]
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = "images/OSELOGO.png"
+html_logo = "images/OSELOGO_white_small.png"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
